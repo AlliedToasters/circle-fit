49c7e533f96b1a5933646b6b6fcf065bc35f7963
Marian <mail@marian42.de>
2019-04-20 17:12:02 +0200

    Remove semicolons

--- circle_fit/circle_fit.py
+++ circle_fit/circle_fit.py
@@ -22,10 +22,10 @@ def sigma(coords, x, y, r):
     dx, dy, sum_ = 0., 0., 0.
 
     for i in range(len(coords)):
-        dx = coords[i][1] - x;
-        dy = coords[i][0] - y;
-        sum_ += (sqrt(dx*dx+dy*dy) - r)**2;
-    return sqrt(sum_/len(coords));
+        dx = coords[i][1] - x
+        dy = coords[i][0] - y
+        sum_ += (sqrt(dx*dx+dy*dy) - r)**2
+    return sqrt(sum_/len(coords))
 
 def hyper_fit(coords, IterMax=99, verbose=False):
     """
@@ -65,45 +65,45 @@ def hyper_fit(coords, IterMax=99, verbose=False):
     Zi = Xi*Xi + Yi*Yi
 
     #compute moments
-    Mxy = (Xi*Yi).sum()/n;
-    Mxx = (Xi*Xi).sum()/n;
-    Myy = (Yi*Yi).sum()/n;
-    Mxz = (Xi*Zi).sum()/n;
-    Myz = (Yi*Zi).sum()/n;
-    Mzz = (Zi*Zi).sum()/n;
+    Mxy = (Xi*Yi).sum()/n
+    Mxx = (Xi*Xi).sum()/n
+    Myy = (Yi*Yi).sum()/n
+    Mxz = (Xi*Zi).sum()/n
+    Myz = (Yi*Zi).sum()/n
+    Mzz = (Zi*Zi).sum()/n
 
     #computing the coefficients of characteristic polynomial
-    Mz = Mxx + Myy;
-    Cov_xy = Mxx*Myy - Mxy*Mxy;
-    Var_z = Mzz - Mz*Mz;
+    Mz = Mxx + Myy
+    Cov_xy = Mxx*Myy - Mxy*Mxy
+    Var_z = Mzz - Mz*Mz
 
-    A2 = 4*Cov_xy - 3*Mz*Mz - Mzz;
-    A1 = Var_z*Mz + 4.*Cov_xy*Mz - Mxz*Mxz - Myz*Myz;
-    A0 = Mxz*(Mxz*Myy - Myz*Mxy) + Myz*(Myz*Mxx - Mxz*Mxy) - Var_z*Cov_xy;
-    A22 = A2 + A2;
+    A2 = 4*Cov_xy - 3*Mz*Mz - Mzz
+    A1 = Var_z*Mz + 4.*Cov_xy*Mz - Mxz*Mxz - Myz*Myz
+    A0 = Mxz*(Mxz*Myy - Myz*Mxy) + Myz*(Myz*Mxx - Mxz*Mxy) - Var_z*Cov_xy
+    A22 = A2 + A2
 
     #finding the root of the characteristic polynomial
     y = A0
     x = 0.
-    for i in range(IterMax):#(x=0.,y=A0,iter=0; iter<IterMAX; iter++)
-        Dy = A1 + x*(A22 + 16.*x*x);
-        xnew = x - y/Dy;
+    for i in range(IterMax):
+        Dy = A1 + x*(A22 + 16.*x*x)
+        xnew = x - y/Dy
         if xnew == x or not np.isfinite(xnew):
             break
-        ynew = A0 + xnew*(A1 + xnew*(A2 + 4.*xnew*xnew));
+        ynew = A0 + xnew*(A1 + xnew*(A2 + 4.*xnew*xnew))
         if abs(ynew)>=abs(y):
             break
         x, y = xnew, ynew
 
-    det = x*x - x*Mz + Cov_xy;
-    Xcenter = (Mxz*(Myy - x) - Myz*Mxy)/det/2.;
-    Ycenter = (Myz*(Mxx - x) - Mxz*Mxy)/det/2.;
+    det = x*x - x*Mz + Cov_xy
+    Xcenter = (Mxz*(Myy - x) - Myz*Mxy)/det/2.
+    Ycenter = (Myz*(Mxx - x) - Mxz*Mxy)/det/2.
 
-    x = Xcenter + X.mean();
-    y = Ycenter + Y.mean();
-    r = sqrt(abs(Xcenter**2 + Ycenter**2 + Mz));
-    s = sigma(coords,x,y,r);
-    iter_ = i;
+    x = Xcenter + X.mean()
+    y = Ycenter + Y.mean()
+    r = sqrt(abs(Xcenter**2 + Ycenter**2 + Mz))
+    s = sigma(coords,x,y,r)
+    iter_ = i
     if verbose:
         print('Regression complete in {} iterations.'.format(iter_))
         print('Sigma computed: ', s)
@@ -144,7 +144,7 @@ def least_squares_circle(coords):
     x_m = np.mean(x)
     y_m = np.mean(y)
     center_estimate = x_m, y_m
-    center, ier = optimize.leastsq(f, center_estimate, args=(x,y))
+    center, _ = optimize.leastsq(f, center_estimate, args=(x,y))
     xc, yc = center
     Ri       = calc_R(x, y, *center)
     R        = Ri.mean()
